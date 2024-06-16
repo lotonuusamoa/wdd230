86ec03cb81b4c572b2ec0dc8e75945242cdd40de
lotonuusamoa <andre@lotonuu.com>
2024-06-15 18:09:21 -1100

    Chamber 6-15-24

---
+++ week01/scripts/app.js
@@ -0,0 +1,47 @@
+
+const date1 = document.querySelector("#date1");
+const message = document.querySelector("#emessage");
+
+
+try {
+	const options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+		year: "numeric"
+	};
+	date1.innerHTML = `Today is <span class="highlight">${new Date().toLocaleDateString("en-UK", options)}</span>!`;
+} catch (e) {
+	console.log("Error with code or your browser does not support Locale");
+}
+
+
+const daynames = [
+	"Sunday",
+	"Monday",
+	"Tuesday",
+	"Wednesday",
+	"Thursday",
+	"Friday",
+	"Saturday"
+];
+const months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"October",
+	"November",
+	"December"
+];
+const d = new Date();
+const dayName = daynames[d.getDay()];
+const monthName = months[d.getMonth()];
+const year = d.getFullYear();
+const fulldate = `${dayName}, ${d.getDate()} ${monthName} ${year}`;
+document.querySelector("#date2").textContent = fulldate;
