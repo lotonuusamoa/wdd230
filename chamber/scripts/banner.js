4d699b1dc476c698c35fa404ffa84b090379a0c3
lotonuusamoa <andre@lotonuu.com>
2024-07-23 03:29:19 -1100

    test

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -2,5 +2,5 @@
 const today = new Date()
 //console.log(today.getDay())
 if (today.getDay() > 4) {
-  document.querySelector('#banner').className = "hide"
+  document.querySelector('https://lotonuusamoa.github.io/wdd230/chamber/images/apia-J.webp').className = "hide"
 }
