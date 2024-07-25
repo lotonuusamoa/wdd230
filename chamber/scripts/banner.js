1f795f4aeb8ac76cdff094eb1632fce517c706af
lotonuusamoa <andre@lotonuu.com>
2024-07-25 09:25:39 -1100

    test

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -1,6 +1,14 @@
 //banner
-const today = new Date()
+//const today = new Date()
 //console.log(today.getDay())
-if (today.getDay() > 4) {
-  document.querySelector('#banner').className = "hide"
+//if (today.getDay() > 4) {
+  //document.querySelector('#banner').className = "hide"
+//}
+
+const today = new Date();
+const dayOfWeek = today.getDay();
+
+// Check if the current day is Friday (5), Saturday (6), or Sunday (0)
+if (dayOfWeek >= 5 || dayOfWeek === 0) {
+  document.querySelector('#banner').className = "hide";
 }
